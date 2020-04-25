56ecb99d6b01037198356f95455fcb210950943a
Jaclyn Wolfe <jwolfe100@ivytech.edu>
2020-04-25 12:41:44 -0400

    M05 bookmarks

---
+++ class_projects/bookmarks.sql
@@ -0,0 +1,18 @@
+CREATE datebase bookmarks;
+USE bookmarks;
+
+CREATE TABLE user (
+username VARCHAR(16) NOT NULL PRIMARY KEY,
+passwd CHAR(40) NOT NULL,
+email VARCHAR(100) NOT NULL );
+
+CREATE TABLE bookmark (
+username VARCHAR(16) NOT NULL,
+bm_URL VARCHAR(255) NOT NULL,
+index (username),
+index (bm_URL),
+PRIMARY KEY(username, bm_URL) );
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON bookmarks.*
+TO bm_user@localhost IDENTIFIED BY 'password';
